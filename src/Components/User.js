44f283476e641e7a97f9eb1a94c18545a7db3c6a
Alexey Beltyukov <lesha.beltyukov@gmail.com>
2023-02-19 18:35:03 +0400

    User component is avaliable! Icons for deleting and editing are added on User.js

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,12 +1,16 @@
 import React from "react";
-
+import { IoCloseCircleSharp, IoHammerSharp } from 'react-icons/io5'
 class User extends React.Component{
+    user = this.props.user
     render(){
-        return(<div key={this.props.user.id} className="apart_user">
-        <h3>Имя: {this.props.user.first_name}</h3>
-        <h3>Фамилия: {this.props.user.second_name}</h3>
-        <h3>Возраст: {this.props.user.age}</h3>
-        <h3>Счастье: {this.props.user.happiness ? "Счастлив": "Несчастлив"}</h3>
+        return(<div className="apart_user"><IoCloseCircleSharp className="icon deleteIcon"/>
+        <IoHammerSharp className="icon editIcon"/>
+
+        <h3>Имя: {this.user.first_name}</h3>
+
+        <h3>Фамилия: {this.user.second_name}</h3>
+        <h3>Возраст: {this.user.age}</h3>
+        <h3>Счастье: {this.user.happiness ? "Счастлив": "Несчастлив"}</h3>
     </div>)
     }
 }
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -3,39 +3,37 @@ import User from "./User";
 
 
 class Users extends React.Component{
-    users = [
-        {
-            id: 1,
-            first_name: 'Alexey',
-            second_name: 'Beltiukov',
-            age: 18,
-            happiness: true
-        },
-        {
-            id: 2,
-            first_name: 'Andrey',
-            second_name: 'Beltiukov',
-            age: 25,
-            happiness: true
-
-        },
-        {
-            id: 3,
-            first_name: 'Artem',
-            second_name: 'Beltiukov',
-            age: 28,
-            happiness: false
-
-        }
-    ]
+    constructor(props){
+        super(props)
+        this.state = {users: [
+            {
+                id: 1,
+                first_name: 'Alexey',
+                second_name: 'Beltiukov',
+                age: 18,
+                happiness: true
+            },
+            {
+                id: 2,
+                first_name: 'Andrey',
+                second_name: 'Beltiukov',
+                age: 25,
+                happiness: true
+    
+            },
+            {
+                id: 3,
+                first_name: 'Artem',
+                second_name: 'Beltiukov',
+                age: 28,
+                happiness: false
+    
+            }
+        ]}
+    }
     render(){
         return (<div>
-            {this.users.map((user) => (<div key={user.id} className="apart_user">
-                <h3>Имя: {user.first_name}</h3>
-                <h3>Фамилия: {user.second_name}</h3>
-                <h3>Возраст: {user.age}</h3>
-                <h3>Счастье: {user.happiness ? "Счастлив": "Несчастлив"}</h3>
-            </div>))}
+            {this.state.users.map((user) => (<User key={user.id} user={user}/>))}
         </div>)
     }
 }
